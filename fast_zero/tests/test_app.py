66820747e79cce5ecde541b8aecff7f00511cc92
Arafat-Ahmad-Sheikh <asterisk287@gmail.com>
2026-01-30 01:32:29 +0530

    bug cleared for inheristance over UserDB schema and tested create_user req

--- fast_zero/tests/test_app.py
+++ fast_zero/tests/test_app.py
@@ -6,6 +6,24 @@ from app.main import app
 def test_root_returns_ok_and_message():
     client = TestClient(app)
     response = client.get("/")
-
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá Mundo!"}
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'alice',
+        'email': 'alice@example.com',
+    }
