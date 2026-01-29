f7e520e47ff74c4e917c6d405a0ad959136322c1
Arafat-Ahmad-Sheikh <asterisk287@gmail.com>
2026-01-30 02:22:13 +0530

    implemented get put delete routes too and tested them using pytest

--- fast_zero/app/main.py
+++ fast_zero/app/main.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
-from fastapi import FastAPI
-from schemas import Message, UserSchema, UserPublic, UserDB
+from fastapi import FastAPI, HTTPException
+from schemas import Message, UserSchema, UserPublic, UserDB, UserList
 
 app = FastAPI()
 
@@ -17,3 +17,26 @@ def create_user(user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
     database.append(user_with_id)
     return user_with_id
+
+
+@app.get("/users/", response_model=UserList)
+def read_users():
+    return {"users": database}
+
+
+@app.put("/users/{user_id}", response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User Not Found")
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+    database[user_id - 1] = user_with_id
+    return user_with_id
+
+
+@app.delete("/users/{user_id}", response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="User Not Found")
+    del database[user_id - 1]
+
+    return {"message": "User Deleted Successfully"}
